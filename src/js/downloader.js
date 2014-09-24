1bf2897db33556bdc808aed45b61cdb67b532da4
Tema <unlucky@inbox.ru>
2014-09-24 21:32:14 +0900

    Нумерация треков в плейлистах и в альбома�

--- src/js/downloader.js
+++ src/js/downloader.js
@@ -7,6 +7,26 @@ downloader.clearPath = function (path) {
     return path.replace(/[\\/:*?"<>|]/g, '_'); // Windows path illegals
 };
 
+downloader.getPrefix = function (i, max) {
+    var prefix = '';
+    max = max.toString();
+    switch (max.length) {
+        case 2:
+            prefix = (i < 10) ? '0' + i : i;
+            break;
+        case 3:
+            prefix = (i < 10) ? '00' + i : ((i < 100) ? '0' + i : i);
+            break;
+        case 4:
+            // надеюсь 9999 хватит
+            prefix = (i < 10) ? '000' + i : ((i < 100) ? '00' + i : ((i < 1000) ? '0' + i : i));
+            break;
+        default:
+            prefix = i;
+    }
+    return prefix + ' - ';
+};
+
 downloader.download = function () {
     var track = downloader.queue.shift();
     if (!track) {
@@ -20,13 +40,16 @@ downloader.download = function () {
         return;
     }
     downloader.activeThreadCount++;
-    if (track.version) {
-        track.title += ' (' + track.version + ')';
-    }
     var artists = track.artists.map(function (artist) {
         return artist.name;
     }).join(', ');
+    if (track.version) {
+        track.title += ' (' + track.version + ')';
+    }
     var savePath = downloader.clearPath(artists + ' - ' + track.title + '.mp3');
+    if (track.namePrefix) {
+        savePath = track.namePrefix + savePath;
+    }
     if (track.saveDir) {
         savePath = track.saveDir + '/' + savePath;
     }
@@ -66,17 +89,22 @@ downloader.downloadAlbum = function (album) {
     var artists = album.artists.map(function (artist) {
         return artist.name;
     }).join(', ');
+    if (album.version) {
+        album.title += ' (' + album.version + ')';
+    }
     var saveDir = downloader.clearPath(artists + ' - ' + album.title);
     if (album.volumes.length > 1) {
         for (var i = 0; i < album.volumes.length; i++) {
-            album.volumes[i].forEach(function (track) {
+            album.volumes[i].forEach(function (track, j) {
                 track.saveDir = saveDir + '/CD' + (i + 1);
+                track.namePrefix = downloader.getPrefix(j + 1, album.volumes[i].length);
             });
             tracks = tracks.concat(album.volumes[i]);
         }
     } else {
-        album.volumes[0].forEach(function (track) {
+        album.volumes[0].forEach(function (track, i) {
             track.saveDir = saveDir;
+            track.namePrefix = downloader.getPrefix(i + 1, album.volumes[0].length);
         });
         tracks = album.volumes[0];
     }
@@ -90,8 +118,9 @@ downloader.downloadAlbum = function (album) {
 };
 
 downloader.downloadPlaylist = function (playlist) {
-    playlist.tracks.forEach(function (track) {
+    playlist.tracks.forEach(function (track, i) {
         track.saveDir = downloader.clearPath(playlist.title);
+        track.namePrefix = downloader.getPrefix(i + 1, playlist.tracks.length);
     });
     downloader.add(playlist.tracks);
 };
